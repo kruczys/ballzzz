d09b3fd66e85e9d8d8988628b66653c88fdc8cdc
Konrad Kreczko <konradkreczko@gmail.com>
2024-12-26 10:13:53 +0100

    feat: Optimized simulation

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -6,64 +6,77 @@
 
 void Simulation::simulate()
 {
+    double gravity_rate_ps = this->count_gravity_acceleration_rate_per_second();
     for (auto &particle : this->particles)
     {
+        double x_speed = particle.get_x_speed();
+        double y_speed = particle.get_y_speed();
+        double x_pos = particle.get_x_pos();
+        double y_pos = particle.get_y_pos();
+        double size = particle.get_size();
+
         // Movement
-        particle.set_x_pos(particle.get_x_pos() + particle.get_x_speed());
-        particle.set_y_pos(particle.get_y_pos() + particle.get_y_speed());
+        particle.set_x_pos(x_pos + x_speed);
+        particle.set_y_pos(y_pos + y_speed);
 
         // Air resistance
-        particle.set_x_speed(particle.get_x_speed() * 0.999);
+        particle.set_x_speed(x_speed * 0.999);
 
         // collision with other particles
         for (auto &other_particle : this->particles)
         {
+            double x_pos_other = other_particle.get_x_pos();
+            double y_pos_other = other_particle.get_y_pos();
+            double x_speed_other = other_particle.get_x_speed();
+            double y_speed_other = other_particle.get_y_speed();
+            double size_other = other_particle.get_size();
+
             if (particle != other_particle)
             {
-                if (distance(particle.get_x_pos(), particle.get_y_pos(), other_particle.get_x_pos(), other_particle.get_y_pos()) < (particle.get_size() + other_particle.get_size()) / 2)
+                if (distance(x_pos, y_pos, x_pos_other, y_pos_other) < (size + size_other) / 2)
                 {
-                    double dx = other_particle.get_x_pos() - particle.get_x_pos();
-                    double dy = other_particle.get_y_pos() - particle.get_y_pos();
+                    double dx = x_pos_other - x_pos;
+                    double dy = y_pos_other - y_pos;
 
                     double dist = std::sqrt(dx * dx + dy * dy);
                     dx /= dist;
                     dy /= dist;
 
-                    double v1 = particle.get_x_speed() * dx + particle.get_y_speed() * dy;
-                    double v2 = other_particle.get_x_speed() * dx + other_particle.get_y_speed() * dy;
+                    double v1 = x_speed * dx + y_speed_other * dy;
+                    double v2 = x_speed_other * dx + y_speed * dy;
 
-                    particle.set_x_speed(particle.get_x_speed() + (v2 - v1) * dx);
-                    particle.set_y_speed(particle.get_y_speed() + (v2 - v1) * dy);
-                    other_particle.set_x_speed(other_particle.get_x_speed() + (v1 - v2) * dx);
-                    other_particle.set_y_speed(other_particle.get_y_speed() + (v1 - v2) * dy);
+                    particle.set_x_speed(x_speed + (v2 - v1) * dx);
+                    particle.set_y_speed(y_speed + (v2 - v1) * dy);
+                    other_particle.set_x_speed(x_speed_other + (v1 - v2) * dx);
+                    other_particle.set_y_speed(y_speed_other + (v1 - v2) * dy);
                 }
             }
         }
 
         // collision with walls
-        if (particle.get_y_pos() > this->y_window - particle.get_size() + 2)
+        if (y_pos > this->y_window - size + 2)
         {
-            particle.set_y_pos(this->y_window - particle.get_size() + 2);
-            particle.set_y_speed(-particle.get_y_speed() * 0.9);
+            particle.set_y_pos(this->y_window - size + 2);
+            particle.set_y_speed(-y_speed * 0.9);
         }
-        if (particle.get_y_pos() < particle.get_size() + 2)
+        if (y_pos < size + 2)
         {
-            particle.set_y_pos(particle.get_size() + 2);
-            particle.set_y_speed(-particle.get_y_speed() * 0.9);
+            particle.set_y_pos(size + 2);
+            particle.set_y_speed(-y_speed * 0.9);
         }
-        if (particle.get_x_pos() > this->x_window - particle.get_size() + 2)
+        if (x_pos > this->x_window - size + 2)
         {
-            particle.set_x_pos(this->x_window - particle.get_size() + 2);
-            particle.set_x_speed(-particle.get_x_speed() * 0.9);
+            particle.set_x_pos(this->x_window - size + 2);
+            particle.set_x_speed(-x_speed * 0.9);
         }
-        if (particle.get_x_pos() < particle.get_size() + 2)
+        if (x_pos < size + 2)
         {
-            particle.set_x_pos(particle.get_size() + 2);
-            particle.set_x_speed(-particle.get_x_speed() * 0.9);
+            particle.set_x_pos(size + 2);
+            particle.set_x_speed(-x_speed * 0.9);
         }
 
         // gravity pull
-        particle.accelerate(0, -this->count_gravity_acceleration_rate_per_second());
+        particle.accelerate(0, -gravity_rate_ps);
     }
 }
 
